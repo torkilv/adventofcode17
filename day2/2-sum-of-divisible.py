d77aa0e906b7d786051d0efe45bcdadd8a49724e
Torkil Vederhus <torkilv@gmail.com>
2017-12-02 17:26:20 +0100

    simpler

--- day2/2-sum-of-divisible.py
+++ day2/2-sum-of-divisible.py
@@ -7,10 +7,9 @@ spreadsheetFile = open("input-2", "r")
 checkSum = 0
 for line in spreadsheetFile:
 	numbers = map(int, line.split())
-	combinations = list(itertools.product(numbers, repeat=2))
+	combinations = list(itertools.permutations(numbers, 2))
 
-	modulos = map(lambda pair: (pair[0] != pair[1]) and (pair[0] % pair[1]), combinations)
-
+	modulos = map(lambda pair: (pair[0] % pair[1]), combinations)
 	divisiblePair = list(combinations)[map(str, modulos).index("0")]
 	checkSum += divisiblePair[0] / divisiblePair[1]
 
