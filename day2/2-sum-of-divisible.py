2c5ec23444a24e9700bef1dd39bec19ecb421475
Torkil Vederhus <torkilv@gmail.com>
2017-12-02 17:47:55 +0100

    using next

--- day2/2-sum-of-divisible.py
+++ day2/2-sum-of-divisible.py
@@ -9,7 +9,10 @@ for line in spreadsheetFile:
 	numbers = map(int, line.split())
 	combinations = itertools.permutations(numbers, 2)
 	
-	divisiblePair = next(pair for paior in combinations if pair[0] % pair[1]):
+	divisiblePair = next(pair for pair in combinations if pair[0] % pair[1]==0)
+
+	checkSum += divisiblePair[0]/divisiblePair[1]
+	
 
 
 print checkSum
