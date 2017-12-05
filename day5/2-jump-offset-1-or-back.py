87c61379ad762c0eeccc7a23fc43afeaca0c4c18
Torkil Vederhus <torkilv@gmail.com>
2017-12-05 13:01:38 +0100

    Separated in two files

--- day5/2-jump-offset-1-or-back.py
+++ day5/2-jump-offset-1-or-back.py
@@ -8,7 +8,12 @@ steps = 0
 while current_step > -1 and current_step < len(instructions):
 	steps += 1
 	current_instruction = instructions[current_step]
-	instructions[current_step] += 1
+	
+
+	if current_instruction < 3:
+		instructions[current_step] += 1
+	else:
+		instructions[current_step] -= 1
 
 	current_step += current_instruction
 
